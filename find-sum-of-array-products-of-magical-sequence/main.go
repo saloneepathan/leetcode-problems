f9fa59593e8186416b4a48210a9335941c3e8c7d
Salonee Pathan <saloneepathan@gmail.com>
2025-10-13 00:06:46 +0530

    Hard-#3539

--- find-sum-of-array-products-of-magical-sequence/main.go
+++ find-sum-of-array-products-of-magical-sequence/main.go
@@ -1,93 +1,80 @@
-package main
-
-import (
-	"fmt"
-	"math/big"
-)
+func quickmul(x, y, mod int64) int64 {
+	res, cur := int64(1), x%mod
+	for y > 0 {
+		if y&1 == 1 {
+			res = res * cur % mod
+		}
+		y >>= 1
+		cur = cur * cur % mod
+	}
+	return res
+}
 
 func magicalSum(m int, k int, nums []int) int {
-	const MOD = 1e9 + 7
-	n := len(nums)
-
-	// dp[pos][sum] = sum of products where:
-	// - We've filled pos positions
-	// - The sum 2^seq[0] + 2^seq[1] + ... equals 'sum'
-	// Note: sum can be very large, but we only care about set bit count
-
-	// Since we can have up to m=30 identical indices, the max sum is
-	// 30 * 2^49 which is huge. Instead, track (pos, setbitcount)
-	// But we need to know which specific bits are set to correctly add new powers
-
-	// Better: use big integers or track actual bit values
-	// Use map with big.Int or string representation
-
-	memo := make(map[string]int64)
-
-	keyFunc := func(pos int, sum *big.Int) string {
-		return fmt.Sprintf("%d:%s", pos, sum.String())
+	mod := int64(1000000007)
+	fac := make([]int64, m+1)
+	fac[0] = 1
+	for i := 1; i <= m; i++ {
+		fac[i] = fac[i-1] * int64(i) % mod
+	}
+	ifac := make([]int64, m+1)
+	ifac[0] = 1
+	ifac[1] = 1
+	for i := 2; i <= m; i++ {
+		ifac[i] = quickmul(int64(i), mod-2, mod)
+	}
+	for i := 2; i <= m; i++ {
+		ifac[i] = ifac[i-1] * ifac[i] % mod
 	}
 
-	var solve func(int, *big.Int) int64
-	solve = func(pos int, sum *big.Int) int64 {
-		// Count set bits in sum
-		bits := sum.BitLen()
-		if sum.Sign() > 0 {
-			bits = 0
-			for i := 0; i < sum.BitLen(); i++ {
-				if sum.Bit(i) == 1 {
-					bits++
+	numsPower := make([][]int64, len(nums))
+	for i := range nums {
+		numsPower[i] = make([]int64, m+1)
+		numsPower[i][0] = 1
+		for j := 1; j <= m; j++ {
+			numsPower[i][j] = numsPower[i][j-1] * int64(nums[i]) % mod
+		}
+	}
+
+	f := make([][][][]int64, len(nums))
+	for i := range nums {
+		f[i] = make([][][]int64, m+1)
+		for j := 0; j <= m; j++ {
+			f[i][j] = make([][]int64, m*2+1)
+			for p := 0; p <= m*2; p++ {
+				f[i][j][p] = make([]int64, k+1)
+			}
+		}
+	}
+
+	for j := 0; j <= m; j++ {
+		f[0][j][j][0] = numsPower[0][j] * ifac[j] % mod
+	}
+	for i := 0; i+1 < len(nums); i++ {
+		for j := 0; j <= m; j++ {
+			for p := 0; p <= m*2; p++ {
+				for q := 0; q <= k; q++ {
+					q2 := p%2 + q
+					if q2 > k {
+						break
+					}
+					for r := 0; r+j <= m; r++ {
+						p2 := p/2 + r
+						f[i+1][j+r][p2][q2] += f[i][j][p][q] * numsPower[i+1][r] % mod * ifac[r] % mod
+						f[i+1][j+r][p2][q2] %= mod
+					}
 				}
 			}
 		}
-
-		// Base case
-		if pos == m {
-			if bits == k {
-				return 1
-			}
-			return 0
-		}
-
-		// Pruning
-		if bits > k {
-			return 0
-		}
-		if bits+m-pos < k {
-			return 0
-		}
-
-		// Memo check
-		memoKey := keyFunc(pos, sum)
-		if val, exists := memo[memoKey]; exists {
-			return val
-		}
-
-		result := int64(0)
-
-		// Try each index
-		for i := 0; i < n; i++ {
-			// Add 2^i to sum
-			newSum := new(big.Int).Set(sum)
-			power := big.NewInt(1)
-			power.Lsh(power, uint(i))
-			newSum.Add(newSum, power)
-
-			ways := solve(pos+1, newSum)
-			contribution := (int64(nums[i]) * ways) % MOD
-			result = (result + contribution) % MOD
-		}
-
-		memo[memoKey] = result
-		return result
 	}
 
-	initialSum := big.NewInt(0)
-	return int(solve(0, initialSum) % MOD)
-}
-
-func main() {
-	fmt.Println(magicalSum(3, 2, []int{33}))                         // 35937
-	fmt.Println(magicalSum(5, 5, []int{1, 10, 100, 10000, 1000000})) // 991600007
-	fmt.Println(magicalSum(2, 2, []int{5, 4, 3, 2, 1}))              // 170
-	fmt.Println(magicalSum(1, 1, []int{28}))                         // 28
+	res := int64(0)
+	for p := 0; p <= m*2; p++ {
+		for q := 0; q <= k; q++ {
+			if bits.OnesCount(uint(p))+q == k {
+				res = (res + f[len(nums)-1][m][p][q]*fac[m]%mod) % mod
+			}
+		}
+	}
+	return int(res)
 }
