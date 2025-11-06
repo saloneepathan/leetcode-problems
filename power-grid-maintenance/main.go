fc402ce9be7f974c70cf3d952d69c522da1cdf1c
Salonee Pathan <saloneepathan@gmail.com>
2025-11-06 16:15:53 +0530

    Medium-#3607

--- power-grid-maintenance/main.go
+++ power-grid-maintenance/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-// ---------- DSU (Disjoint Set Union) ----------
+// ---------- DSU ----------
 type DSU struct {
 	parent, rank []int
 }
@@ -16,7 +16,7 @@ func NewDSU(n int) *DSU {
 	for i := 1; i <= n; i++ {
 		p[i] = i
 	}
-	return &DSU{parent: p, rank: r}
+	return &DSU{p, r}
 }
 
 func (d *DSU) Find(x int) int {
@@ -41,17 +41,15 @@ func (d *DSU) Union(x, y int) {
 	}
 }
 
-// ---------- Min-Heap for each grid ----------
+// ---------- Min-Heap ----------
 type MinHeap []int
 
 func (h MinHeap) Len() int           { return len(h) }
 func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
 func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
 func (h *MinHeap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
-
 func (h *MinHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
@@ -60,37 +58,35 @@ func (h *MinHeap) Pop() interface{} {
 	return x
 }
 
-// ---------- Main Function ----------
+// ---------- Main ----------
 func processQueries(c int, connections [][]int, queries [][]int) []int {
 	dsu := NewDSU(c)
-
-	// Step 1: Build DSU from connections
-	for _, conn := range connections {
-		dsu.Union(conn[0], conn[1])
-	}
-
-	// Step 2: Build heaps for each component
-	heaps := make(map[int]*MinHeap)
 	online := make([]bool, c+1)
 	for i := 1; i <= c; i++ {
 		online[i] = true
 	}
 
+	// Build connectivity
+	for _, e := range connections {
+		dsu.Union(e[0], e[1])
+	}
+
+	// Build heaps for each connected component
+	heaps := make([]*MinHeap, c+1)
 	for i := 1; i <= c; i++ {
 		root := dsu.Find(i)
-		if _, ok := heaps[root]; !ok {
+		if heaps[root] == nil {
 			heaps[root] = &MinHeap{}
 		}
 		heap.Push(heaps[root], i)
 	}
 
-	// Step 3: Process queries
 	res := []int{}
 
 	for _, q := range queries {
 		t, x := q[0], q[1]
 
-		if t == 1 { // Maintenance check
+		if t == 1 { // Maintenance query
 			root := dsu.Find(x)
 			if online[x] {
 				res = append(res, x)
@@ -98,19 +94,19 @@ func processQueries(c int, connections [][]int, queries [][]int) []int {
 			}
 
 			h := heaps[root]
-			for h.Len() > 0 {
+			for h != nil && h.Len() > 0 {
 				top := (*h)[0]
 				if online[top] {
 					res = append(res, top)
 					break
 				}
-				heap.Pop(h) // lazy delete offline ones
+				heap.Pop(h) // remove offline ones
 			}
-			if h.Len() == 0 {
+			if h == nil || h.Len() == 0 {
 				res = append(res, -1)
 			}
 
-		} else if t == 2 { // Station goes offline
+		} else { // t == 2, take station offline
 			online[x] = false
 		}
 	}
@@ -118,10 +114,10 @@ func processQueries(c int, connections [][]int, queries [][]int) []int {
 	return res
 }
 
-// ---------- Example Usage ----------
+// ---------- Example ----------
 func main() {
 	c := 5
 	connections := [][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}}
 	queries := [][]int{{1, 3}, {2, 1}, {1, 1}, {2, 2}, {1, 2}}
-	fmt.Println(processQueries(c, connections, queries)) // Output: [3 2 3]
+	fmt.Println(processQueries(c, connections, queries)) // [3 2 3]
 }
