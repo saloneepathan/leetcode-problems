0b48152d50d66d0ea8006540fdf8103469d5f1c9
Salonee Pathan <saloneepathan@gmail.com>
2026-01-27 00:00:05 +0530

    Easy-#1200

--- minimum-absolute-difference/main.go
+++ minimum-absolute-difference/main.go
@@ -1,4 +1,9 @@
-import "sort"
+package main
+
+import (
+	"fmt"
+	"sort"
+)
 
 func minimumAbsDifference(arr []int) [][]int {
 	sort.Ints(arr)
@@ -6,7 +11,7 @@ func minimumAbsDifference(arr []int) [][]int {
 	minDiff := int(1e9)
 	n := len(arr)
 
-	// Step 1: Find minimum difference
+	// Find minimum difference
 	for i := 1; i < n; i++ {
 		diff := arr[i] - arr[i-1]
 		if diff < minDiff {
@@ -14,7 +19,7 @@ func minimumAbsDifference(arr []int) [][]int {
 		}
 	}
 
-	// Step 2: Collect pairs with minDiff
+	// Collect pairs with minimum difference
 	result := [][]int{}
 	for i := 1; i < n; i++ {
 		if arr[i]-arr[i-1] == minDiff {
@@ -23,4 +28,14 @@ func minimumAbsDifference(arr []int) [][]int {
 	}
 
 	return result
+}
+
+func main() {
+	arr1 := []int{4, 2, 1, 3}
+	arr2 := []int{1, 3, 6, 10, 15}
+	arr3 := []int{3, 8, -10, 23, 19, -4, -14, 27}
+
+	fmt.Println(minimumAbsDifference(arr1)) // [[1 2] [2 3] [3 4]]
+	fmt.Println(minimumAbsDifference(arr2)) // [[1 3]]
+	fmt.Println(minimumAbsDifference(arr3)) // [[-14 -10] [19 23] [23 27]]
 }
