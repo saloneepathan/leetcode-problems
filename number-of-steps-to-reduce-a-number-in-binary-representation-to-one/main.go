3bb9a8cdff7a29a137f467ccc4acdee677053a8b
Salonee Pathan <saloneepathan@gmail.com>
2026-02-27 00:02:16 +0530

    Medium-#1404

--- number-of-steps-to-reduce-a-number-in-binary-representation-to-one/main.go
+++ number-of-steps-to-reduce-a-number-in-binary-representation-to-one/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+)
+
+func numSteps(s string) int {
+	steps := 0
+	carry := 0
+	n := len(s)
+
+	// Traverse from right to left (excluding the first bit)
+	for i := n - 1; i > 0; i-- {
+		bit := int(s[i] - '0')
+
+		if bit+carry == 1 {
+			// Odd → add 1 (creates carry), then divide
+			steps += 2
+			carry = 1
+		} else {
+			// Even → just divide by 2
+			steps += 1
+		}
+	}
+
+	// If there's leftover carry, add one more step
+	return steps + carry
+}
+
+func main() {
+	// Testcases
+	fmt.Println(numSteps("1101")) // Output: 6
+	fmt.Println(numSteps("10"))   // Output: 1
+	fmt.Println(numSteps("1"))    // Output: 0
+}
