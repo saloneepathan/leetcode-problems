333a4d187a31016da6c8090b74915a3571b46df2
Salonee Pathan <saloneepathan@gmail.com>
2026-04-04 10:28:06 +0530

    Hard-#3661

--- maximum-walls-destroyed-by-robots/main.go
+++ maximum-walls-destroyed-by-robots/main.go
@@ -1,37 +1,92 @@
-class Solution {
-    const int LL = 0, LR = 1, RL = 2, RR = 3; 
-public:
-    int maxWalls(vector<int>& robots, vector<int>& distance, vector<int>& walls) {
-        robots.push_back(-1), robots.push_back(2e9);
-        distance.push_back(0), distance.push_back(0);
-        vector<int> rIndices(robots.size());
-        iota(rIndices.begin(), rIndices.end(), 0);
-        sort(rIndices.begin(), rIndices.end(), [&](int idx1, int idx2) {
-            return robots[idx1] < robots[idx2];
-        });
-        int wallSize = walls.size();
-        sort(walls.begin(), walls.end());
-        int curr = 0;
-        int lbot = rIndices[curr], rbot = rIndices[curr + 1];
-        // dp[LL/LR/RL/RR]: the directions of the robots around the wall fire to
-        int dp[4] = {0};
-        for (int i = 0; i < wallSize; i++) {
-            while (walls[i] > robots[rbot]) {
-                ++curr;
-                lbot = rIndices[curr];
-                rbot = rIndices[curr + 1];
-                int maxL = max(dp[LL], dp[RL]);
-                int maxR = max(dp[LR], dp[RR]);
-                dp[LL] = dp[LR] = maxL;
-                dp[RL] = dp[RR] = maxR;
-            }
-            dp[LL] += walls[i] >= robots[rbot] - distance[rbot];
-            dp[LR] += walls[i] == robots[rbot];
-            dp[RL] += walls[i] <= robots[lbot] + distance[lbot] 
-                        || walls[i] >= robots[rbot] - distance[rbot];
-            dp[RR] += walls[i] <= robots[lbot] + distance[lbot]
-                        || walls[i] == robots[rbot];
-        }
-        return *max_element(dp, dp + 4);
-    }
-};
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxWalls(robots []int, distance []int, walls []int) int {
+	const (
+		LL = 0
+		LR = 1
+		RL = 2
+		RR = 3
+	)
+
+	// Append sentinel robots
+	robots = append(robots, -1)
+	robots = append(robots, 2000000000)
+	distance = append(distance, 0)
+	distance = append(distance, 0)
+
+	n := len(robots)
+
+	// rIndices
+	rIndices := make([]int, n)
+	for i := 0; i < n; i++ {
+		rIndices[i] = i
+	}
+
+	sort.Slice(rIndices, func(i, j int) bool {
+		return robots[rIndices[i]] < robots[rIndices[j]]
+	})
+
+	sort.Ints(walls)
+
+	curr := 0
+	lbot := rIndices[curr]
+	rbot := rIndices[curr+1]
+
+	dp := make([]int, 4)
+
+	for i := 0; i < len(walls); i++ {
+		for walls[i] > robots[rbot] {
+			curr++
+			lbot = rIndices[curr]
+			rbot = rIndices[curr+1]
+
+			maxL := max(dp[LL], dp[RL])
+			maxR := max(dp[LR], dp[RR])
+
+			dp[LL] = maxL
+			dp[LR] = maxL
+			dp[RL] = maxR
+			dp[RR] = maxR
+		}
+
+		if walls[i] >= robots[rbot]-distance[rbot] {
+			dp[LL]++
+		}
+
+		if walls[i] == robots[rbot] {
+			dp[LR]++
+		}
+
+		if walls[i] <= robots[lbot]+distance[lbot] ||
+			walls[i] >= robots[rbot]-distance[rbot] {
+			dp[RL]++
+		}
+
+		if walls[i] <= robots[lbot]+distance[lbot] ||
+			walls[i] == robots[rbot] {
+			dp[RR]++
+		}
+	}
+
+	return max(max(dp[0], dp[1]), max(dp[2], dp[3]))
+}
+
+func main() {
+	robots := []int{1, 4}
+	distance := []int{1, 1}
+	walls := []int{2, 3, 4}
+
+	fmt.Println(maxWalls(robots, distance, walls))
+}
