41a8019a0a7448bda177cb8f2d3a8ea5e4522765
Salonee Pathan <saloneepathan@gmail.com>
2026-07-27 23:44:41 +0530

    Easy-#1464

---
+++ maximum-product-of-two-elements-in-an-array/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func maxProduct(nums []int) int {
+	max1, max2 := 0, 0
+
+	for _, num := range nums {
+		if num > max1 {
+			max2 = max1
+			max1 = num
+		} else if num > max2 {
+			max2 = num
+		}
+	}
+
+	return (max1 - 1) * (max2 - 1)
+}
+
+func main() {
+	fmt.Println(maxProduct([]int{3, 4, 5, 2})) // 12
+	fmt.Println(maxProduct([]int{1, 5, 4, 5})) // 16
+	fmt.Println(maxProduct([]int{3, 7}))       // 12
+}
