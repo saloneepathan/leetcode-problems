a8330ee6bd46f928b3aabe0782a3b2b8ca989d4a
Salonee Pathan <57287517+saloneepathan@users.noreply.github.com>
2026-09-02 23:53:51 +0530

    Easy-#3875

---
+++ construct-uniform-parity-array-i/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func uniformArray(nums1 []int) bool {
+	return true
+}
+
+func main() {
+	nums1 := []int{2, 3}
+
+	result := uniformArray(nums1)
+
+	fmt.Println(result)
+}
